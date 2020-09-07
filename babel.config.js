53ed28813cbdbc5bfbfaf9a2f9dd05d3d8fa7ccd
William Di Pasquale <wdp@dipasqualew.com>
2020-09-07 19:18:46 +0100

    Update Integration and E2E tests

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
+const VUE_CLI_BABEL_PRESET = '@vue/cli-plugin-babel/preset';
+const JEST_BABEL_PRESET =     ['@babel/preset-env', { targets: { node: 'current' } } ];
+const JEST_TYPESCRIPT_PRESET = '@babel/preset-typescript';
+
 const vueConfig = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    VUE_CLI_BABEL_PRESET,
+    JEST_BABEL_PRESET,
+    JEST_TYPESCRIPT_PRESET,
   ],
 };
 
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,23 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+const shared = {
   moduleNameMapper: {
     '~/(.*)$': '<rootDir>/$1',
   },
+  setupFiles: [
+    '<rootDir>/tests/setup.ts',
+  ],
+};
+
+module.exports = {
+  projects: [
+    {
+      ...shared,
+      displayName: 'unit',
+      testMatch: ['<rootDir>/tests/unit/**/*.ts'],
+    },
+    {
+      ...shared,
+      displayName: 'integration',
+      testMatch: ['<rootDir>/tests/integration/**/*.ts'],
+    },
+  ],
 };
