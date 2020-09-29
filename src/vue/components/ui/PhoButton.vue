61239d846d3fd43d1513e5dbe3d0cce60de56113
yurianxdev <bryangarzon22@gmail.com>
2020-09-29 16:28:19 -0500

    Tailwind CSS Migration: PhoButton component Fixes #102

--- src/vue/components/ui/PhoButton.vue
+++ src/vue/components/ui/PhoButton.vue
@@ -1,12 +1,25 @@
 <template>
-  <button type="button" @click="onClick"><slot /></button>
+  <button
+    :class="{'bg-gray-300 text-gray-900': disabled,
+             'bg-gray-800 text-white hover:shadow-lg hover hover:bg-gray-700': !disabled}"
+    :disabled="disabled"
+    class="rounded-lg font-bold px-6 py-2"
+    type="button"
+    @click="onClick">
+    <slot/>
+  </button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
-
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
   setup(_props, context) {
     const onClick = ($event: MouseEvent) => {
       return context.emit('click', $event);
