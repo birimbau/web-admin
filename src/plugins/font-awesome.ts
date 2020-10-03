f4dcf06c802b6920b5131d09eaa6d14d527b3e90
birimbau <tiagompbneto@gmail.com>
2020-10-03 17:23:37 +0200

    Fixed code review comments

--- src/plugins/font-awesome.ts
+++ src/plugins/font-awesome.ts
@@ -1,8 +1,23 @@
 import Vue from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faUserSecret);
+/* 
+    To Use Font awesome you must import each icon and add it to the library, like:
+    
+    import { library } from '@fortawesome/fontawesome-svg-core';
+    import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+    library.add(faUserSecret);
+
+    If you desire to import all icons at once you can use:
+
+    // Solid Icons
+    import { fas } from '@fortawesome/free-solid-svg-icons'
+    library.add(fas)
+    
+    // Brand Icons
+    import { fab } from '@fortawesome/free-solid-svg-icons'
+    library.add(fav)
+
+*/
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
