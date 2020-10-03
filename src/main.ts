0d8fb96cfd649382978a98bdfb1c2406d563c0b5
birimbau <tiagompbneto@gmail.com>
2020-10-03 18:41:31 +0200

    Added Font awesome package

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import '~/src/plugins/sentry';
 import '~/src/assets/scss/main.scss';
 import { appLoaded } from '~/src/state/service';
 import { router } from '~/src/vue/router';
+import FontAwesomeIcon from '~/src/plugins/font-awesome';
 import App from '~/src/App.vue';
 
 export const main = async () => {
   await createApp(App)
     .use(router)
+    .component('FontAwesomeIcon', FontAwesomeIcon)
     .mount('#app');
 
   appLoaded.value = true;
