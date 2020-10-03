325a754bb193365f590dfa9d7324536e751d9328
birimbau <tiagompbneto@gmail.com>
2020-10-03 18:48:41 +0200

    Added comments on font awesome plugin

--- src/plugins/font-awesome.ts
+++ src/plugins/font-awesome.ts
@@ -1,6 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-library.add(fas);
+
+/* 
+    To Use Font awesome you must import each icon and add it to the library, like:
+    
+    import { library } from '@fortawesome/fontawesome-svg-core';
+    import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+    library.add(faUserSecret);
+
+    If you desire to import all icons at once you can use:
+
+    import { fas } from '@fortawesome/free-solid-svg-icons'
+    library.add(fas)
+
+*/
 
 export default FontAwesomeIcon;
